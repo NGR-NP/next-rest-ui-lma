03c6ab8956e3fbb4267d2ac916853f34295aa8a7
NGR NP <ngr112001@gmail.com>
2022-04-15 18:17:47 +0545

    commit

---
+++ components/Featured.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+import styles from "../styles/Featured.module.css";
+
+const Featured = () => {
+  const images = [
+    "/img/Featured.png",
+    "/img/Featured2.png",
+    "/img/Featured3.png",
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.arrowContainer}>
+        <Image src="/img/arrowl.png" alt="arrowl" layout="fill" />
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          {images.map((img, i) => (
+            <Image
+              src={img}
+              key={i}
+              alt="featured"
+              layout="fill"
+              objectFit="contain"
+            />
+          ))}
+        </div>
+      </div>
+      <div className={styles.arrowContainer}>
+        <Image src="/img/arrowr.png" alt="arrow" layout="fill" />
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
---
+++ components/Navbar.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image
+            src="/img/telephone.png"
+            width="32"
+            height="32"
+            alt="telephone"
+          />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>98 12 345 678</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" width="160px" height="69px" alt="logo" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" width="30px " height="30px" alt="cart" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
